d26354694aec7907ab2d4991f7cb4b9e9e72aa14
Roman <vehsylamor@gmail.com>
2025-06-29 23:07:39 +0300

    edit processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,24 @@
-def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
-    Фильтрует список операций по значению ключа 'state'.
+    Принимает список словарей. Имеет необязательный параметр
+    для порядка сортировки (по умолчанию EXECUTED — выполнено,
+    либо CANCELED — отменено).  Возвращает новый список,
+    отсортированный по статусу (state).
     """
-    list_1 = []
-    for transaction in transactions:
-        if transaction.get("state") == state:
-            list_1.append(transaction)
-    return list_1
+    sorted_list = []
+    for operate in operations:
+        if operate.get("state") == state:
+            sorted_list.append(operate)
+    return sorted_list
 
 
-def sort_by_date(transactions: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(operations: list[dict], reverse=True) -> list[dict]:
     """
-    Фильтрует список операций по значению ключа 'date'.
+    Принимает список словарей. Имеет необязательный параметр `reverse`,
+    задающий порядок сортировки:
+    reverse=True (по умолчанию) — сортировка по убыванию даты,
+    reverse=False — сортировка по возрастанию даты.
+    Возвращает новый список, отсортированный по ключу - date.
     """
-    sorted_list = sorted(transactions, key=lambda x: x["date"], reverse=reverse)
+    sorted_list = sorted(operations, key=lambda x: x["date"], reverse=reverse)
     return sorted_list
