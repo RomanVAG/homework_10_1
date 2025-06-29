51464233ed5cad76f1939e75f06b74588e54f17d
Roman <vehsylamor@gmail.com>
2025-06-29 19:16:27 +0300

    modify file processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,16 +10,8 @@ def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[d
 
 
 def sort_by_date(transactions: list[dict],  reverse=True) -> list[dict]:
+    """
+    Фильтрует список операций по значению ключа 'date'.
+    """
     sorted_list = sorted(transactions, key=lambda x: x['date'], reverse=reverse)
     return sorted_list
-
-
-transactions = [
-{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-print(filter_by_state(transactions,'CANCELED'))
-print(sort_by_date(transactions))
